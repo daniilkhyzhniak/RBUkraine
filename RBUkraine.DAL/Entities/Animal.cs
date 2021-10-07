380374d718e9e9f5a5c8d5ef01eb49a98a8bc31b
daniilkhyzhniak <daniil.khyzhniak@nure.ua>
2021-10-07 11:52:18 +0300

    Add animal images table

--- RBUkraine.DAL/Entities/Animal.cs
+++ RBUkraine.DAL/Entities/Animal.cs
@@ -1,9 +1,14 @@
 ﻿using RBUkraine.DAL.Entities.Base;
+using System.Collections.Generic;
 
 namespace RBUkraine.DAL.Entities
 {
     public class Animal : BaseEntity
     {
         public string Name { get; set; }
+
+        public AnimalDetails AnimalDetails { get; set; }
+
+        public ICollection<AnimalImage> AnimalImages { get; set; }
     }
 }
---
+++ RBUkraine.DAL/Entities/AnimalImage.cs
@@ -0,0 +1,13 @@
+﻿using RBUkraine.DAL.Entities.Base;
+
+namespace RBUkraine.DAL.Entities
+{
+    public class AnimalImage : BaseEntity
+    {
+        public string Title { get; set; }
+
+        public string Data { get; set; }
+
+        public int AnimalId { get; set; }
+    }
+}
