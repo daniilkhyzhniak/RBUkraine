28cea2220af10e76656c6e6103517483221b55c8
daniilkhyzhniak <daniil.khyzhniak@nure.ua>
2021-11-05 21:04:59 +0200

    Add missing mappings

--- RBUkraine.BLL/Models/CharityEvent/CharityEventEditorModel.cs
+++ RBUkraine.BLL/Models/CharityEvent/CharityEventEditorModel.cs
@@ -4,7 +4,6 @@ namespace RBUkraine.BLL.Models.CharityEvent
 {
     public class CharityEventEditorModel
     {
-        public int Id { get; set; }
         public string Name { get; set; }
 
         public string Description { get; set; }
